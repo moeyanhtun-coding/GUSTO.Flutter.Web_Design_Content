55078a893b3d1cbf798a264728cda4e2c9043f88
moeyanhtun-coding <moeyan227970@gmail.com>
2024-07-22 18:48:21 +0630

    [Complete] New Project Create

---
+++ android/app/src/main/kotlin/com/example/flutter_web_responsive/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_web_responsive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
