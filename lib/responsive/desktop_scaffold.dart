6f0c8757af186ebea1544e808e34697ce46af36f
moeyanhtun-coding <moeyan227970@gmail.com>
2024-07-23 11:33:58 +0630

    [Add] Desktop_Web_Layout

---
+++ lib/responsive/desktop_scaffold.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DesktopScaffold extends StatefulWidget {
+  const DesktopScaffold({super.key});
+
+  @override
+  State<DesktopScaffold> createState() => _DesktopScaffoldState();
+}
+
+class _DesktopScaffoldState extends State<DesktopScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      body: Container(
+        child: Text(MediaQuery.sizeOf(context).width.toString()),
+      ),
+    );
+  }
+}
