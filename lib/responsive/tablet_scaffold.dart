51e2d5ed4e65146bf5ed61112e5abeea4ef3fdcb
moeyanhtun-coding <moeyan227970@gmail.com>
2024-07-23 11:33:43 +0630

    [Add] Tablet_Layout

---
+++ lib/responsive/tablet_scaffold.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TabletScaffold extends StatefulWidget {
+  const TabletScaffold({super.key});
+
+  @override
+  State<TabletScaffold> createState() => _TabletScaffoldState();
+}
+
+class _TabletScaffoldState extends State<TabletScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: Container(
+        child: Text(MediaQuery.sizeOf(context).width.toString()),
+      ),
+    );
+  }
+}
