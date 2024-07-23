c0fc78cd789e7b02ac9463b1b658dc4ded74d87d
moeyanhtun-coding <moeyan227970@gmail.com>
2024-07-23 11:33:30 +0630

    [Add] Mobile_layout

---
+++ lib/responsive/mobile_scaffold.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MobileScaffold extends StatefulWidget {
+  const MobileScaffold({super.key});
+
+  @override
+  State<MobileScaffold> createState() => _MobileScaffoldState();
+}
+
+class _MobileScaffoldState extends State<MobileScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Container(
+        child: Text(MediaQuery.sizeOf(context).width.toString()),
+      ),
+    );
+  }
+}
