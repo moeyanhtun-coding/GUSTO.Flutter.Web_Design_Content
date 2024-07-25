677b246f06d478cd539306b64d44ee90cd76e320
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-25 21:09:35 +0630

    -

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_any_logo
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterAnyLogoPlugin.register(with: registry.registrar(forPlugin: "FlutterAnyLogoPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
